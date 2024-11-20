d0fb127e0954a74a9e213fd3b578ff671604f106
Andre Machado <andreluiz.dagamamachado@student.sl.on.ca>
2024-11-20 15:03:02 -0500

    Add eventTypes field and related methods to EventService
    Included a new field 'eventTypes' in the EventService class and added methods to get this field. Also added a constructor to initialize 'eventTypes' and integrated a method to add events to the eventList.

--- src/main/java/comp31/java_assgm/services/EventService.java
+++ src/main/java/comp31/java_assgm/services/EventService.java
@@ -1,11 +1,34 @@
 package comp31.java_assgm.services;
 
+import comp31.java_assgm.model.Event;
 import comp31.java_assgm.model.EventList;
+import comp31.java_assgm.model.EventTypes;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
 
+
+
     EventList eventList;
+    EventTypes eventTypes;
+
+    public EventList getEventList() {
+        return eventList;
+    }
+
+    public EventTypes getEventTypes() {
+        return eventTypes;
+    }
+
+    EventService(EventList eventList, EventTypes eventTypes) {
+        this.eventList = eventList;
+        this.eventTypes = eventTypes;
+    }
+
+    public void addEventList(Event event){
+        eventList.add(event);
+    }
+
 
 }
